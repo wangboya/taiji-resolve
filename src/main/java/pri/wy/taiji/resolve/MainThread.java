a3095039a36fa2a3ad58274fad3b2a9eb2c678d7
wangboya <wangboya.private@hotmail.com>
2020-05-26 22:38:38 +0800

    添加入口函数说明

--- src/main/java/pri/wy/taiji/resolve/MainThread.java
+++ src/main/java/pri/wy/taiji/resolve/MainThread.java
@@ -3,6 +3,15 @@ package pri.wy.taiji.resolve;
 import static pri.wy.taiji.resolve.Utils.valOf;
 
 public class MainThread {
+     /**
+     * 第一个参数是当前的布局,1代表黑色,0代表白色,顺时针例如
+     * 10001000,表示总共有8个,其中第一个,第五个是黑色
+     * 第二个参数是当前的线条布局,为了方便,从第一个线条开始,1代表有线条,0代表无线条,顺时针例如
+     * 11010000,表示第1,2,4的位置有线条
+     * 如果结果求出来了,
+     * 那么只要看'@'符号的数值,表示把第一个线条放在第几个位置
+     * @param args
+     */
     public static void main(String[] args) {
         long t = System.currentTimeMillis();
         String start = args[0];
